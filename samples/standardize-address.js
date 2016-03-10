386e6fa4fc6e530c7a6bd2201641c44ced8ba72e
Mark Headd <mheadd@gmail.com>
2016-03-10 13:42:52 -0500

    Update sample

--- samples/standardize-address.js
+++ samples/standardize-address.js
@@ -1,10 +1,12 @@
 var property = require('../index');
 
-property.getStandardizedAddress('1234 market', function(error, response) {
+var address = process.argv[2] || '1234 Market Street';
+
+property.getStandardizedAddress(address, function(error, response) {
 	if(!error) {
 		console.log(response);
 	}
 	else {
-		console.log(error.message)
+		console.log(error);
 	}
 });
