624ba77faddd656c9b63a4cc04763e15430862aa
Mark Headd <mheadd@gmail.com>
2016-11-14 14:59:59 -0500

    Fixes #1

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
 var request = require('request');
-var async = require('async');
 
-// URLs to City of Philadelphia address and property APIs.
-var address_url = 'https://api.phila.gov/ulrs/v3/addresses/';
-var property_url = 'https://data.phila.gov/resource/bz79-67af.json?address_id=';
+// URL to City of Philadelphia address and property APIs.
+var address_url = 'https://api.phila.gov/ais/v1/addresses/';
 
-// Returns JSON array containing details for a specific property.
 exports.getPropertyInfo = function(address, callback) {
-	async.waterfall([
-			async.apply(standardizeAddress, address),
-			getAddressDetails
-		],
-		function generateResponse(error, body) {
-			if(!error) {
-				callback(null, JSON.parse(body));
-			}
-			else {
-				callback({ message: error.message });
-			}
-		});
-}
-
-// Returns a JSON object with standardized address and location information for a property.
-exports.getStandardizedAddress = function(address, callback) {
-	standardizeAddress(address, callback); 
-}
-
-// Standardize address and location information for a property.
-function standardizeAddress(address, callback) {
-	var url = address_url + encodeURIComponent(address) + '?format=json';
+	var url = address_url + encodeURIComponent(address);
 	request(url, function(err, resp, body) {
-		var error = resp.statusCode != 200 ? new Error('Unable to standardize address') : err;
-		callback(error, body);
+		var error = resp.statusCode != 200 ? new Error('Unable to retrieve address info.') : err;
+		if(!error) {
+			callback(null, JSON.parse(body));
+		}
+		else {
+			callback({ message: error.message });
+		}
 	});
-}
 
-// Get property details for an address.
-function getAddressDetails(body, callback) {
-	var addresses = JSON.parse(body).addresses
-	if(addresses.length == 0) {
-		callback(new Error('No property information for that address'), null);
-	}
-	else {
-		var url = property_url + encodeURIComponent(addresses[0].standardizedAddress);
-		request(url, function(err, resp, body) {
-			callback(err, body);
-		});
-	}
-}
+};
--- test/phl-property.js
+++ test/phl-property.js
@@ -8,37 +8,20 @@ describe('phl-property', function() {
 	// Tests for getPropertyInfo method.
 	describe('Property information', function() {
 		it('Should return property information', function(done) {
-			property.getPropertyInfo('1234 market street', function(error, response) {
+			property.getPropertyInfo('1234 Market Street', function(error, response) {
 				expect(error).to.be.null;
-				expect(response[0].address_id).to.equal('1234 MARKET ST');
+				expect(response.features[0].properties.street_address).to.equal('1234 MARKET ST');
 				done();
 			});
 		});
 		it('Should return an error if address is invalid', function(done) {
 			property.getPropertyInfo('1234 fake street', function(error, response) {
 				expect(error).to.not.be.null;
-				expect(error.message).to.equal('Unable to standardize address');
+				expect(error.message).to.equal('Unable to retrieve address info.');
 				done();
 			});
 	  	});
 	});
 
-	// Tests for getStandardizedAddress method.
-	describe('Standardize address', function() {
-		it('Should return address information', function(done) {
-			property.getStandardizedAddress('1234 market street', function(error, response) {
-				expect(error).to.be.null;
-				expect(JSON.parse(response).addresses[0].standardizedAddress).to.equal('1234 MARKET ST');
-				done();
-			});
-	  	});
-	  	it('Should return an error if address is invalid', function(done) {
-			property.getStandardizedAddress('1234 fake street', function(error, response) {
-				expect(error).to.not.be.null;
-				expect(error.message).to.equal('Unable to standardize address');
-				done();
-			});
-	  	});
-	})
 
 });
